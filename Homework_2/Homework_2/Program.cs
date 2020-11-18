c83d44585fbc9a40e280ad4e9388635b270bc1bd
DESKTOP-99K5BEK\HP <khayo2000@gmail.com>
2020-11-18 22:18:34 +0200

    realization

--- Homework_2/Homework_2/Program.cs
+++ Homework_2/Homework_2/Program.cs
@@ -10,7 +10,25 @@ namespace Homework_2
     {
         static void Main(string[] args)
         {
+            string boo = "boo";
+            string hello = "hello";
+            MyColection<string> vs = new MyColection<string>();
+            vs.Add(hello);
+            vs.Add(boo);
+            vs.Add("row");
 
+            vs.Remove(boo);
+
+            if (vs.Contains(hello))
+            {
+                vs.RemoveAt(0);
+            }
+            foreach (var item in vs)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
         }
     }
 }
